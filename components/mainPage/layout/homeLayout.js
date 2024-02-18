5b1be040e5fa0758507c419291da28394cfd5b27
Motifff <57866475+Motifff@users.noreply.github.com>
2024-02-18 17:35:54 +0000

    update

--- components/mainPage/layout/homeLayout.js
+++ components/mainPage/layout/homeLayout.js
@@ -66,6 +66,7 @@ export default function HomeLayout() {
       <animated.div className="Homepage"
         style={{
           position:"sticky",
+          top:0,
           width: "100vw",
           padding: 24,
           display: 'flex',
