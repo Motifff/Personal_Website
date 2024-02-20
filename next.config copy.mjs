b7371273c6eac491ab212ba16ce0a975d5f1a716
Motifff <57866475+Motifff@users.noreply.github.com>
2024-02-20 11:28:22 +0000

    update

--- next.config copy.mjs
+++ next.config copy.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {  
+const nextConfig = {
+    /**
+     * Enable static exports for the App Router.
+     *
+     * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+     */
+    output: "export",
+  
     /**
      * Set base path. This is the slug of your GitHub repository.
      *
