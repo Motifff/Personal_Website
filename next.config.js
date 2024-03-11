7985e19ce49fc417b893cc45a72b84dae04a5859
Motifff <57866475+Motifff@users.noreply.github.com>
2024-03-11 12:29:24 +0000

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-const devConfig = require('./next.config.dev.js');
-const prodConfig = require('./next.config.prod.js');
-
-module.exports = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    //remember to unquote for github pages
+    basePath: "/Personal_Website",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+    images: {
+      unoptimized: false,
+    },
+  };
+  
+  module.exports = nextConfig;
