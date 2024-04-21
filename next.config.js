29cc00e0afbdf9b9e975b0cdf583f50f375770f4
Motifff <57866475+Motifff@users.noreply.github.com>
2024-04-21 18:42:19 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     //remember to unquote for github pages
-    //basePath: "/Personal_Website",
+    basePath: "/Personal_Website",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
     images: {
